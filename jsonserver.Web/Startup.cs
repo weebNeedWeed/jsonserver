9727c8a12f8afff599bebd7f225de9b8a3ec816b
Weed <rivenmle@gmail.com>
2022-04-14 12:59:11 +0700

    Updated production's configuration

--- jsonserver.Web/Startup.cs
+++ jsonserver.Web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace jsonserver.Web
 {
@@ -46,6 +47,13 @@ namespace jsonserver.Web
                 options.Cookie.IsEssential = true;
                 options.Cookie.Name = "JsonServer.Cookie";
             });
+
+            services.AddCors(x => x.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyHeader()
+                       .AllowAnyMethod();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,7 +68,15 @@ namespace jsonserver.Web
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
+
+                app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                });
             }
+
+            app.UseCors("MyPolicy");
 
             app.UseNotFoundPage("/404");
 
