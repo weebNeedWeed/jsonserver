b34c8853f7e19f3c13f380c537546e82a8449f88
Weed <rivenmle@gmail.com>
2022-04-14 14:36:41 +0700

    Updated configuration

--- jsonserver.Web/Startup.cs
+++ jsonserver.Web/Startup.cs
@@ -68,7 +68,6 @@ namespace jsonserver.Web
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-                app.UseHttpsRedirection();
 
                 app.UseForwardedHeaders(new ForwardedHeadersOptions
                 {
